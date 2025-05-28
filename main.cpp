1f7c15623b164012d67396bb2121f50ac5452fda
bru651 <borbace55@gmail.com>
2025-05-28 21:36:19 +0200

    Działa

--- main.cpp
+++ main.cpp
@@ -5,70 +5,73 @@
 //#include <cmath>
 #include <ctime>
 #include <iostream>
-std::vector<sf::Vector2i> checkNeigbours(std::vector<std::vector<int>> const &oilNew,int x, int y) {
+
+sf::Vector2i GetEdgePos(int size) {
+    int r = rand() % 4;
+    if (r == 0) {
+        return sf::Vector2i(0, rand() % size);
+    }
+    else if (r == 1) {
+        return sf::Vector2i(size - 1, rand() % size);
+    }
+    else if (r == 2) {
+        return sf::Vector2i(rand() % size, 0);
+    }
+    else if (r == 3) {
+        return sf::Vector2i(rand() % size, size - 1);
+    }
+}
+
+bool checkNeigbours(std::vector<std::vector<int>> const &oilNew,int x, int y) {
     int size = oilNew.size();
-    std::vector<sf::Vector2i> neighs;
-    std::vector<sf::Vector2i> borders{ sf::Vector2i (0, 1),sf::Vector2i(0, -1),sf::Vector2i(-1, 0),sf::Vector2i(1, 0) };
+    //std::vector<sf::Vector2i> neighs;
+    std::vector<sf::Vector2i> borders{ sf::Vector2i(0, 0), sf::Vector2i (0, 1),sf::Vector2i(0, -1),sf::Vector2i(-1, 0),sf::Vector2i(1, 0) };
     int rnx, rny;
-    for (int n = 0; n < 4; n++) {
+    for (int n = 0; n < 5; n++) {
         rnx = x + borders[n].x; rny = y + borders[n].y;
         if (rnx  < 0 || rny < 0 || rnx > size - 1 || rny > size - 1) continue;
         else {
-            if (oilNew[rnx][rny]!=0)continue;
-            else neighs.push_back(borders[n]);
+            if (oilNew[rnx][rny]!=1)continue;
+            else return 1;
         }
     }
-    return neighs;
+    return 0;
 }
 
-void StainUpdate(std::vector<std::vector<int>>& oilNew, int surf) {
+void StainUpdate(std::vector<std::vector<int>>& oilNew, std::vector<sf::Vector2i>& particles) {
     int size = oilNew.size();   // Za³o¿enie ¿e tablica ma kszta³t kwadratu
-    std::vector<sf::Vector2i> neighbours;
-    sf::Vector2i growth;
-    int rnx, rny, pass;
-    int growing = rand() % surf;    //Która komórka roœnie
-    pass = 0; // Pominiête powiechrznie
-    for (int x = 0; x < size; x++) {
-        for (int y = 0; y < size; y++) {
-            if (oilNew[x][y] == 1) {
-                if (!(pass<growing)) {
-                    //std::cout << "CHECK" << std::endl;
-                    neighbours = checkNeigbours(oilNew, x, y);
-                    //std::cout << "UP1" << std::endl;
-                    growth = neighbours[rand()%neighbours.size()];
-                    rnx = x + growth.x; rny = y + growth.y;
-                    //std::cout << "UP2" << std::endl;
-                    oilNew[rnx][rny] = 1;
-                    std::vector<sf::Vector2i> borders{ sf::Vector2i(rnx, rny), sf::Vector2i(rnx, rny+1),sf::Vector2i(rnx, rny -1),sf::Vector2i(rnx -1, rny),sf::Vector2i(rnx + 1, rny) };
-                    //std::cout << "UP3" << std::endl;
-                    for (int n = 0; n < 5; n++) {
-                        
-                        if (borders[n].x  < 0 || borders[n].y < 0 || borders[n].x > size - 1 || borders[n].y > size - 1 || oilNew[borders[n].x][borders[n].y]<1) continue;
-                        neighbours = checkNeigbours(oilNew, borders[n].x, borders[n].y);
-                        if (neighbours.size() < 1)oilNew[borders[n].x][borders[n].y] = 2;
-                    }
-                    return;
-                }
-                pass += 1;
-            }
+    std::vector<sf::Vector2i> neighbours{ sf::Vector2i(0, 1),sf::Vector2i(0, -1),sf::Vector2i(-1, 0),sf::Vector2i(1, 0)};
+    sf::Vector2i move;
+    int rnx, rny;
+    for (int i = 0; i < particles.size(); i++) {
+        //std::cout << "CHECK 1" << std::endl;
+        if (checkNeigbours(oilNew, particles[i].x, particles[i].y)) {// SprawdŸ czy styka
+            //std::cout << "CHECK 2" << std::endl;
+            if (oilNew[particles[i].x][particles[i].y] != 1)oilNew[particles[i].x][particles[i].y] = 2;
+            //std::cout << "CHECK 3" << std::endl;
+            particles[i] = GetEdgePos(size);
+        }
+        move = neighbours[rand()%4];
+        rnx = particles[i].x + move.x; rny = particles[i].y + move.y;
+        if (rnx  < 0 || rny < 0 || rnx > size - 1 || rny > size - 1) continue;
+        //std::cout << "CHECK 4" << std::endl;
+        particles[i] = sf::Vector2i(rnx, rny);  // Ruch
+        //std::cout << "CHECK 5" << std::endl;
+        if (checkNeigbours(oilNew, particles[i].x, particles[i].y)) {// SprawdŸ czy styka
+            //std::cout << "CHECK 6" << std::endl;
+            if (oilNew[particles[i].x][particles[i].y] != 1)oilNew[particles[i].x][particles[i].y] = 2;
+            //std::cout << "CHECK 7" << std::endl;
+            particles[i] = GetEdgePos(size);
         }
     }
-
-}
-
-int SurfaceCount(std::vector<std::vector<int>> const& oilNew) {
-    int size = oilNew.size();   // Za³o¿enie ¿e tablica ma kszta³t kwadratu
-    int pass;
-    pass = 0; // Pominiête powiechrznie
+    //std::cout << "CHECK 8" << std::endl;
+    // Zamieñ 2(Nowe) na 1(Istniej¹ce)
     for (int x = 0; x < size; x++) {
         for (int y = 0; y < size; y++) {
-            if (oilNew[x][y] == 1) {
-                
-                pass += 1;
-            }
+            if (oilNew[x][y] > 1)oilNew[x][y] = 1;
         }
     }
-    return pass;
+    //std::cout << "CHECK 9" << std::endl;
 }
 
 
@@ -78,7 +81,7 @@ int main()
     // Ustawienia
     int displaySize = 800;
     bool pause = true;
-    int iterations = 5000;     // Iloœæ iteracji
+    int iterations = 250000;     // Iloœæ iteracji
     int curIteration = 1;     // Obecna iteracja
     //int probability = 500;  // Od 0 do 1000
     bool display = true; // Czy ma rysowaæ gry
@@ -87,13 +90,16 @@ int main()
     float blocksize = static_cast<float>(displaySize) / size;  // Graficzna wielkoœæ pola
 
     // Zbiorniki
-    int surface = 1;
     //std::vector<std::vector<bool>> oil(size, std::vector<bool>(size, false));
+    std::vector<sf::Vector2i> particles(size*2, sf::Vector2i(0, 0));
+    for (int i = 0; i < particles.size(); i++) {
+        particles[i] = GetEdgePos(size);
+    }
     std::vector<std::vector<int>> oilNew(size, std::vector<int>(size, 0));
     oilNew[size / 2][size / 2] = 1;
 
     // Grafika
-    sf::RenderWindow window(sf::VideoMode(displaySize, displaySize), "KLastr Edena");
+    sf::RenderWindow window(sf::VideoMode(displaySize, displaySize), "KLastr DLA");
     //window.setFramerateLimit(30);
     sf::RectangleShape shape(sf::Vector2f(blocksize, blocksize));
     shape.setFillColor(sf::Color::Green);
@@ -116,12 +122,15 @@ int main()
                 if (event.key.code == sf::Keyboard::R) {    // Iloœæ iteracji
                     display = !display;
                 }
+                if (event.key.code == sf::Keyboard::I) {    // Iloœæ kroków
+                    std::cout << "Kroki: "<< curIteration << " / " << iterations << std::endl;
+                }
             }
         }
-        if (!pause&&surface!=0) {
+        if (!pause) {
             curIteration += 1;
-            StainUpdate(oilNew, surface);
-            surface = SurfaceCount(oilNew);
+            StainUpdate(oilNew,particles);
+            //pause = true;
         }
         if (iterations == curIteration) {
                 pause = true;
@@ -132,11 +141,12 @@ int main()
 
         if (display) {
             window.clear();
+            shape.setFillColor(sf::Color::Red);
+            // Rysuje Klastr
             for (int x = 0; x < size; x++) {
                 for (int y = 0; y < size; y++) {
-                    if (oilNew[x][y]!=0) {
-                        shape.setFillColor(sf::Color::Green);
-                        if (oilNew[x][y] < 2)shape.setFillColor(sf::Color::Red);
+                    if (oilNew[x][y] > 0) {
+                        
                         //std::cout << "X: "<< x<<" Y: "<<y << std::endl;
                         shape.setPosition(sf::Vector2f(blocksize * x, blocksize * y));
                         window.draw(shape);
@@ -145,7 +155,14 @@ int main()
                     //else std::cout << "Nay" << std::endl;
                 }
             }
-
+            //Rysuje niektóre cz¹stki
+            shape.setFillColor(sf::Color::Green);
+            for (int i = 0; i < particles.size(); i++) {
+                if (i % 25 < 1) {
+                    shape.setPosition(sf::Vector2f(blocksize * particles[i].x, blocksize * particles[i].y));
+                    window.draw(shape);
+                }
+            }
             window.display();
         }//*/
     }
